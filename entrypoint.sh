8ef3043936bd756ddc61c11e0110c9250cad868e
Tristan <starcraft66@gmail.com>
2024-02-09 15:23:40 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -117,6 +117,9 @@ RUN dotnet publish -c Release -o out
 FROM mcr.microsoft.com/dotnet/aspnet:8.0
 WORKDIR /App
 COPY --from=build-env /App/out .
+# App Service
+ENV ASPNETCORE_URLS=http://+:80
+ENV ASPNETCORE_ENVIRONMENT=Development
 ENTRYPOINT ["dotnet", "$SERVICE_NAME.dll"]
 
 EOF
